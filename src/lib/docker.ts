9bcf03d01b6b0f2ee135ad4c0f019ac80703ae20
Lee Norris <lee.norris@arcadia.io>
2021-09-22 10:36:06 -0400

    chore: first

---
+++ src/lib/docker.ts
@@ -0,0 +1,14 @@
+import { ExecOptions } from "child_process";
+import { BaseEncodingOptions } from "fs";
+import { execRx } from "./exec-rx";
+
+export class Docker {
+  constructor(
+    public readonly containerName: string,
+    public readonly binaryName: string,
+  ) { }
+
+  execRx(cmd: string, options?: BaseEncodingOptions & ExecOptions, emitStdErr=true) {
+    return execRx(`docker exec ${this.containerName} ${this.binaryName} ${cmd}`, options, emitStdErr);
+  }
+}
