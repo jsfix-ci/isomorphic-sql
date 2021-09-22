e3f0f56c4e52aca8fdb05ca7733b0e34c9c32254
Lee Norris <lee.norris@arcadia.io>
2021-09-22 14:56:46 -0400

    feat: smeta queries

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ export * from './lib/function/as-date';
 export * from './lib/function/grouper';
 
 export * from './lib/denorm';
+
+export * from './lib/system/get-columns';
+export * from './lib/system/get-defs';
+export * from './lib/system/get-tables';
