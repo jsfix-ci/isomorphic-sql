7c8a8b8f328f187f78bdba640fdd83cd1e7bdc07
Lee Norris <lee.norris@arcadia.io>
2021-09-22 19:19:36 -0400

    chore: clean up get-defs

--- src/lib/system/get-defs.ts
+++ src/lib/system/get-defs.ts
@@ -1,16 +1,16 @@
-import { columnColumnNames as columns } from "../constant/column-column-names";
-import { csv, newline } from "../constant/delims";
-const alias = 'c';
+import { columnColumnNames } from "../constant/column-column-names";
+import { tableColumnNames } from '../constant/table-column-names';
+import { newline } from "../constant/delims";
+const column = 'c';
+const table = 't';
 
 export const getDefs = (schemaname = 'public') => ({
     sql: [
-        `SELECT ${columns.join(csv)} from (`,
-        `   SELECT ${columns.map(c => `${alias}.${c}`)}`,
-        `   FROM pg_catalog.pg_tables t`,
-        `   INNER JOIN information_schema.columns ${alias} on ${alias}.table_name = t.tablename`,
+        `   SELECT ${columnColumnNames.map(c => `${column}.${c}`)}, ${tableColumnNames.map(t => `${table}.${t}`)}`,
+        `   FROM pg_catalog.pg_tables ${table}`,
+        `   INNER JOIN information_schema.columns ${column} on ${column}.table_name = ${table}.tablename`,
         `   WHERE schemaname = '${schemaname}'`,
         `   ORDER BY tablename`,
-        `) def`
     ].join(newline),
-    columns
+    columns: [...columnColumnNames, ...tableColumnNames]
 });
