3eaaae325930644a77cdaca96ba1eb371fd4b516
icedlee337 <lee.e.norris@gmail.com>
2022-05-01 18:10:28 -0400

    feat: unique constraint

--- src/mssql/table.spec.ts
+++ src/mssql/table.spec.ts
@@ -2,22 +2,24 @@ import { Table } from './table';
 
 describe(Table.name, () => {
     it('scripts a table definition with no constraints', () => {
-        const table = new Table('food')
+        const table = Table.named('food')
             .column('id', 'int', [1,1], true)
             .column('name', 'nvarchar(255)')
             .column('inventory_id', 'int');            
 
-        expect(table.toScript()).toMatchSnapshot();
+        expect(table.create()).toMatchSnapshot();
     });
 
     it('scripts a table definition with constraints', () => {
-        const table = new Table('food')
+        const table = Table.named('food')
             .column('id', 'int')
             .column('name', 'nvarchar(255)')
+            .column('plu', 'nvarchar(255)')
             .column('inventory_id', 'int')
             .pk('id')
-            .fk('inventory_id', 'inventory', 'id');
+            .fk('inventory_id', 'inventory', 'id')
+            .unique('name', 'plu');
 
-        expect(table.toScript()).toMatchSnapshot();
+        expect(table.create()).toMatchSnapshot();
     });
 });
--- src/mssql/table.ts
+++ src/mssql/table.ts
@@ -1,4 +1,4 @@
-import { toForeignKeyConstraint, toPrimaryKeyConstraint } from ".";
+import { toForeignKeyConstraint, toPrimaryKeyConstraint, toUniqueConstraint } from ".";
 
 const tab = '  ';
 
@@ -6,7 +6,11 @@ export class Table {
     private columns: { name: string, dataType: string, primaryKey?: boolean, identity?: number[] }[] = [];
     private constraints: string[] = [];
 
-    constructor(public name: string) { }
+    private constructor(public name: string) { }
+
+    static named (name: string) {
+        return new Table(name);
+    }
 
     column(name: string, dataType: string, identity?, primaryKey: boolean = false) {
         this.columns.push({ name, dataType, primaryKey, identity  });
@@ -23,7 +27,12 @@ export class Table {
         return this;
     }
 
-    toScript() {
+    unique(...columns: string[]) {
+        this.constraints.push(toUniqueConstraint(this.name, columns));
+        return this;
+    }
+
+    create() {
         return `CREATE TABLE ${this.name} (
 ${this.columns.map(c => 
     `${tab}${c.name} ${c.dataType} ${c.primaryKey ? 'PRIMARY KEY' : ''} ${c.identity ? `IDENTITY(${c.identity.join(', ')})` : ''}`
